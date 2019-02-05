3b9d6d62a6aaf82e0c2167c59ea94221721426d1
shatyaki_dutt@hotmail.com <shatyaki_dutt@hotmail.com>
2019-02-05 09:31:58 +0530

    Finished ref , uncontrolled component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Header from "./components/layout/Header";
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
+import AddContact1 from "./components/contacts/AddContact1";
 import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,7 +15,8 @@ class App extends Component {
         <div className="App">
           <Header branding={branding} />
           <div className="container">
-            <AddContact />
+            {/* <AddContact /> */}
+            <AddContact1 />
             <Contacts />
           </div>
         </div>
---
+++ src/components/contacts/AddContact1.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+
+class AddContact1 extends Component {
+  constructor(props) {
+    super(props);
+    this.nameInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.phoneInput = React.createRef();
+  }
+  static defaultProps = {
+    name: "Fred Smith",
+    email: "Fred@gamial.com",
+    phone: "2344556789"
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const contact = {
+      name: this.nameInput.current.value,
+      email: this.emailInput.current.value,
+      phone: this.phoneInput.current.value
+    };
+
+    console.log(contact);
+  };
+
+  render() {
+    const { name, email, phone } = this.props;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contactt</div>
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control form-control-lg"
+                placeholder="Enter name"
+                defaultValue={name}
+                ref={this.nameInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control form-control-lg"
+                placeholder="Enter email"
+                defaultValue={email}
+                ref={this.emailInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                className="form-control form-control-lg"
+                placeholder="Enter phone number"
+                defaultValue={phone}
+                ref={this.phoneInput}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-block btn-light"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddContact1;
